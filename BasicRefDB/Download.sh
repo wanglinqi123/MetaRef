36a8720f2e7a97f2d024e4702aabbabc80ce5d93
wanglinqi123 <95482744+wanglinqi123@users.noreply.github.com>
2023-03-06 16:52:40 +0800

    Update Download.sh

--- BasicRefDB/Download.sh
+++ BasicRefDB/Download.sh
@@ -6,15 +6,17 @@
 
 if [ ! -d output ];then mkdir -p output;fi
 
+### Step1: Download reference genomes from GeneBank.
 for line in `cat Env_db_6k.ref.acc.list`;do
-### Download reference genomes from GeneBank.
 GenomePath=`esearch -db assembly -query $line|esummary|xtract -pattern DocumentSummary -element FtpPath_GenBank|head -n 1|awk -F "/" '{gsub(/ftp:/,"http:"); print $0"/"$NF"_genomic.fna.gz"}'`
 wget -nc -q ${GenomePath}
 
-### Replace the gaps in the genome with N base and join all segments into one long, complete sequence to construct the basic reference database.
+### Step2: Replace the gaps in the genome with N base and join all segments into one long, complete sequence to construct the basic reference database.
 GenomeName=`echo $GenomePath|awk -F "/" '{print $(NF-1)}'`
 zcat ${GenomeName}_genomic.fna.gz|sed '1d'|sed '/^>/cNNNNNNNNNN'|sed '1 i\>'${GenomeName}'' >> output/Env_db_6k.DB.fa
 echo ""${GenomeName}" finished."
-
 rm -f ${GenomeName}_genomic.fna.gz
 done
+
+### Step3: Build reference index for the basic reference database. This step will generate five files: *.amb, *.ann, *.bwt, *.pac, *.sa.
+bwa index output/Env_db_6k.DB.fa
